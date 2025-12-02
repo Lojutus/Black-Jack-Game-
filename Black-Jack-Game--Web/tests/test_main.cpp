ed5407dc9bbebf7739c75b9275a01c4713b36070
Lojutus <lojutuselmejor@gmail.com>
2025-12-02 22:45:24 +0000

    Fix names

--- Black-Jack-Game--Web/tests/test_main.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-#include <thread>
-#include <chrono>
-#include "../Console.h"
-//CENTRO DE TESTEO UNITARIO
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
--- Black-Jack-Game--Web/web.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "httplib.h"
-
-// Registra las rutas en el servidor (implementado en web.cpp)
-void definirRutas(httplib::Server &svr);
